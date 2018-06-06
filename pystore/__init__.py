8f964460680a2c37a7c2073aa8194014c7698e98
ran <ran@aroussi.com>
2018-06-06 13:19:22 +0300

    added get_path() method

--- pystore/__init__.py
+++ pystore/__init__.py
@@ -20,12 +20,12 @@
 
 from .store import store
 from .utils import (
-    PATH, set_path,
+    set_path, get_path,
     list_stores, delete_store, delete_stores
 )
 
 __version__ = "0.0.10"
 __author__ = "Ran Aroussi"
 
-__all__ = ['store', 'PATH', 'set_path',
+__all__ = ['store', 'get_path', 'set_path',
            'list_stores', 'delete_store', 'delete_stores']
