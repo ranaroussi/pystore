66d684408057e10e5d819d9a31175b1b8dd42009
ran <ran@aroussi.com>
2018-09-06 23:31:45 +0300

    Switched to dtype_str instead of str(dtype)

--- pystore/item.py
+++ pystore/item.py
@@ -56,8 +56,8 @@ class Item(object):
     def to_pandas(self, parse_dates=True):
         df = self.data.compute()
 
-        if parse_dates and "datetime" not in str(df.index.dtype):
-            if str(df.index.dtype) == 'float64':
+        if parse_dates and "datetime" not in df.index.dtype_str:
+            if df.index.dtype_str == 'float64':
                 df.index = pd.to_datetime(df.index, unit='s',
                                           infer_datetime_format=True)
             else:
